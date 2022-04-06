b56e437717d7ad80a9961bd6102e4a4bc16db64e
Java Software <pj.midgard@yahoo.com>
2022-04-06 21:23:24 +0100

    Update Spring-97.py

--- Spring-97.py
+++ Spring-97.py
@@ -362,7 +362,7 @@ class compression:
                                             
                                             
                                             else:
-                                                    er=ei%100
+                                                    er=ei%1000
                                                     if er==0:
                                                             T1=0
                                                             T2=0
