8c77b17a28a5f8278f576f9df0b14efa70f7fbc2
Java Software <pj.midgard@yahoo.com>
2022-04-09 16:17:31 +0100

    Update Spring-97-A.py

--- Spring-97-A.py
+++ Spring-97-A.py
@@ -304,9 +304,12 @@ class compression:
                                             
                                             
 
-                                            if number_predict[0:1]=="1" and Spin==0:
+                                            if number_predict[0:2]=="11" and Spin==0 or number_predict[0:2]=="01" and Spin==0 or number_predict[0:2]=="10" and Spin==0:
+                                                    number_predict1="1"+number_predict[2:8]
+                                                 
+                                                 
                                                     
-                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict
+                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
                                                     Spin=1
 
 
