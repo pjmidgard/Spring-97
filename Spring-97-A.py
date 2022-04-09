d16a120dff907e1118efbcc718330b746f378dbe
Java Software <pj.midgard@yahoo.com>
2022-04-09 13:11:04 +0100

    Update Spring-97-A.py

--- Spring-97-A.py
+++ Spring-97-A.py
@@ -290,12 +290,13 @@ class compression:
                                             
 
                                             if number_predict[0:2]!="11" and Spin==0:
+                                                    
                                                     Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict
 
 
                                             else:
-                                                    number_predict2="0"+number_predict[2:8]
-                                                    if number_predict2[0:2]=="01" and Spin==0 or number_predict2[0:2]=="00" and Spin==0:
+                                                    
+                                                    if number_predict[0:2]=="01" and Spin==0 or number_predict[0:2]=="00" and Spin==0:
                                                             
                                                             Spin=1
                                                             number_predict1="10"+number_predict
@@ -305,14 +306,14 @@ class compression:
                                                             
                                                     elif   Spin==1:
                                                         
-                                                                                                           Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict
+                                                            Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict
                                                         
 
   
                                                     else:
                                                             number_predict1="0"+number_predict[2:8]
                                                                     
-                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
+                                                            Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
                                                     
                                             ei=ei+8
                                             
