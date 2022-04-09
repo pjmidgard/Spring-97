fcfec3a843b8bcaec84acb6c347a12bfc12d8162
Java Software <pj.midgard@yahoo.com>
2022-04-09 13:25:53 +0100

    Add files via upload

--- Spring-97-A.py
+++ Spring-97-A.py
@@ -289,14 +289,14 @@ class compression:
                                             
                                             
 
-                                            if number_predict[0:2]!="11" and Spin==0:
+                                            if number_predict[0:2]=="10" and Spin==0:
                                                     
-                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict
+                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+"11"+number_predict[2:8]
 
 
                                             else:
                                                     
-                                                    if number_predict[0:2]=="01" and Spin==0 or number_predict[0:2]=="00" and Spin==0:
+                                                    if number_predict[0:2]=="11" and Spin==0 or number_predict[0:2]=="00" and Spin==0:
                                                             
                                                             Spin=1
                                                             number_predict1="10"+number_predict
