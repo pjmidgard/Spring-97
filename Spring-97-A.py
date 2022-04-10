bd4998681a337ae3185a6b783d1605494a0a2d51
Java Software <pj.midgard@yahoo.com>
2022-04-10 12:11:59 +0100

    Add files via upload

--- Spring-97-A.py
+++ Spring-97-A.py
@@ -311,6 +311,7 @@ class compression:
                                                                                        
                                                                                        
                                                             number_predict1="0"+number_predict[2:8]
+                                                            
                                                                     
                                                             Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
 
@@ -540,83 +541,57 @@ class compression:
                                                         Equal_info_between_of_the_cirlce_of_the_file=Equal_info_between_of_the_cirlce_of_the_file[Translate_info_Decimal_2:]
                                                         #Extract ###################################################################################################################
                                                         Number_of_the_file = int(Equal_info_between_of_the_cirlce_of_the_file, 2)
+                                                        Equal_info_between_of_the_cirlce_of_the_file_11=""
+                                                        lenf6=len(Equal_info_between_of_the_cirlce_of_the_file)
                                                             
-                                                        
-                                                        
-                                                            
-                                                        Number_Row1=""
-                                                        Number_Row6=""
-                                                        Number_Row_Count=12
-                                                        Number_Row_Count_str3=""
-                                                        Number_Row=str(Number_of_the_file)
-                                                        Number_Row_Count=Number_Row_Count+1
-                                                        Number_Row_Count_str=str(Number_Row_Count)
-                                                    
-                                                            
-                                                            
-                                                        Row=len(Number_Row)
                                                         ei=0
-                                                        Spin_bits=0
-                                                        while ei<Row:
-                                                                    
-                                                                    Number_Row2=Number_Row[ei:ei+1]
-                                                                    Number_Row4=Number_Row[ei:ei+2]
-                                                                    Number_Row8=Number_Row2[ei+2:ei+3]
-                                                                    Number_Row3=Number_Row2
-                                                                    Number_Row_Count_str3=str(Number_Row_Count)
-                                                                    Number_Row_Count_str=str(Number_Row_Count)
-                                                                    Number_Row_Count_str2=Number_Row_Count_str[1:2]+Number_Row_Count_str[0:1]
+                                                        
 
+                                                        while ei!=lenf6:
+                                                                    number_predict=Equal_info_between_of_the_cirlce_of_the_file[ei:ei+9]
+                                                                    
+                                                                    if number_predict[0:2]=="10":
+                                                                            number_predict1="01"+number_predict[2:8]
+                                                                            Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
+                                                                            ei=ei+8
+                                                                    elif number_predict[0:2]=="11":
+                                                                            number_predict2="1"+number_predict[2:9]
+                                                                            ei=ei+9
+                                                                            
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                            Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict2
+                                                                           
+                                                                            
+                                                                    elif number_predict[0:1]=="0":
+                                                                                                               
+                                                                                                               
+                                                                                    number_predict1="00"+number_predict[1:7]
+                                                                                    
+                                                                                            
+                                                                                    Equal_info_between_of_the_cirlce_of_the_file_11=Equal_info_between_of_the_cirlce_of_the_file_11+number_predict1
+                                                                                    ei=ei+7
 
-                                                                    if ei==0:
-                                                                            Number_Row6=Number_Row[ei:ei+2]
-                                                                            Number_Row1=Number_Row1+Number_Row6
-                                                                    
-                                                                            pin_bits=0
-                                                                            ei=ei+2
-                                                                    
+                                                                            
                                                                     
                                                                     
                                                             
-                                                                    elif Number_Row2[0:1]!=Number_Row_Count_str[0:1]  and ei!=0:
+                                                            
+
+                                                            
+
+                                                            
                                                                 
-                                                                            Number_Row10=Number_Row[ei:ei+2]
-                                                                            Spin_bits=1
-                                                                            
-
-                                                                   
-
-                                                                    elif Number_Row2[0:1]==Number_Row_Count_str[0:1] and ei!=0:
-                                                                
-                                                                            Number_Row6=Number_Row_Count_str
-                                                                            Number_Row1=Number_Row1+Number_Row6
-                                                                            Spin_bits=0
-                                                                            ei=ei+1
-
-                                                                    
-                                                                    if Spin_bits==1:
-                                                                           
-                                                                            Number_Row6=Forest_Extract(Number_Row10,Number_Row_Count)
-                                                                            
-                                                                            Number_Row1=Number_Row1+Number_Row6
-                                                                            pin_bits=0
-                                                                            ei=ei+2
-                                                                    
-                                                                            
-                                                                    Number_Row_Count=Number_Row_Count+1
-                                                                    if Number_Row_Count==88:
-                                                                                    Number_Row_Count=12
-                                                                    Number_Row_Count_str=str(Number_Row_Count)
-                                                                        
-                                                        Number_Row3=Number_Row1
-                                                        #print(Number_Row3)
+                                                                    #print(len(Equal_info_between_of_the_cirlce_of_the_file_17))
+                              
+                                 
                                                         
-                                                        Number_Row_int=int(Number_Row3)
-                                                           
-                                                     
-                                                        Equal_info_between_of_the_cirlce_of_the_file_11=bin(Number_Row_int)[2:]
-
+                                                            
+                                                      
+                                                            
                                                         Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file_11
+                                                        
                                                                         
                                                
                                                         Times_6=Number_add_plus_one
@@ -658,7 +633,7 @@ class compression:
 
                                                         if T!=0:    
          
-                                                                Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file_17[1:(Deep5*8)+1]
+                                                                Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file_17[0:]
                                                         
                                                         lenf14=len(Equal_info_between_of_the_cirlce_of_the_file_17)
                                                         #print(lenf14)
@@ -689,6 +664,7 @@ class compression:
                                                     add_bitszzza=""
                                                     add_bitszs=""
                                                     Equal_info_between_of_the_cirlce_of_the_file_2=Times_6
+                                                    
                                                      
                                                     with open(nameas, "wb") as f2:
                                                     
